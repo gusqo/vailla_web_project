42141c0cb427395d841a8c5c864c0041ca277e0c
gusqo <gusqo002@naver.com>
2026-01-15 19:49:21 +0900

    feature section complete

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,23 @@
-/** Header Change Effect **/
-const header = document.querySelector('header');
-
-const stickyHeader = () => {
-  const scry = window/scrollY;
-  if(scry > 0) {
-    header.classList.add('active');
-} else {
-  header.classList.remove('active');
-}
-
-window.addEventListener('scroll', stickyHeader);
-
 /** Scroll Reveal Effect **/
 const sr = ScrollReveal({
-   reset: false, //스크롤 시 한번만 실행
+  reset: false, //스크롤 시 한번만 실행
 });
- 
 
 sr.reveal('.landing-text-box', {
   duration: 1000,
   origin: 'right',
   distance: '80px',
+});
+
+sr.reveal('.meet-text-box', {
+  duration: 1000,
+  origin: 'right',
+  distance: '40px',
+});
+
+sr.reveal('.meet-wrapper-img, .feature', {
+  duration: 1000,
+  origin: 'right',
+  distance: '40px',
+  interval: 200, //0.2초 간격으로 차례로 효과 진행
 });
